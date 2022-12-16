3bafcff5bd0b8e7f569184e4747fe2f2aa096d5e
Chai <developer.chaitra@gmail.com>
2022-12-17 00:15:25 +1300

    Adding functionality

--- Accordion_Complete/src/Question.js
+++ Accordion_Complete/src/Question.js
@@ -1,7 +1,19 @@
-import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+const Question = ({ id, title, info }) => {
+  const [moreInfo, setMoreInfo] = useState(false);
+
+  return (
+    <article className='question'>
+      <header>
+        <h4>{title}</h4>
+        <button className='btn' onClick={() => setMoreInfo(!moreInfo)}>
+          {moreInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      {moreInfo && <p>{info}</p>}
+    </article>
+  );
 };
 
 export default Question;
