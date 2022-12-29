bc23be59140f60c19a17e958ed128e533b39c9c8
Chai <developer.chaitra@gmail.com>
2022-12-30 10:38:42 +1300

    copy to clipboard and libarary functions

--- color-generator/src/SingleColor.js
+++ color-generator/src/SingleColor.js
@@ -7,14 +7,26 @@ const SingleColor = ({ rgb, weight, index, hexColor }) => {
   const bcg = rgb.join(",");
   const hex = rgbToHex(...rgb);
   const hexValue = `#${hexColor}`;
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+    }, 3000);
+    return () => clearTimeout(timeout);
+  }, [alert]);
   return (
     //using inline css to set up th colors
     <article
       className={`color ${index > 10 && `color-light`}`}
       style={{ backgroundColor: `rgb(${bcg})` }}
+      onClick={() => {
+        setAlert(true);
+        navigator.clipboard.writeText(hexValue);
+      }}
     >
       <p className='percent-value'>{weight}%</p>
       <p className='color-value'>{hexValue}</p>
+      {alert && <p className='alert'>copies to clipboard</p>}
     </article>
   );
 };
