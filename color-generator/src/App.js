41f48057d67d01a667107754316d26673f9f364f
Chai <developer.chaitra@gmail.com>
2022-12-29 20:12:34 +1300

    Using a library to add functionality to the input form

--- color-generator/src/App.js
+++ color-generator/src/App.js
@@ -10,6 +10,14 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    //check whether the value provided is a color or else spit error
+    //value library used here
+    try {
+      let colors = new Values(color).all(10);
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
   };
 
   return (
@@ -22,6 +30,7 @@ function App() {
             value={color}
             onChange={(e) => setColor(e.target.value)}
             placeholder='#f15025'
+            className={`${error ? "error" : null}`}
           />
           <button className='btn' type='submit'>
             submit
