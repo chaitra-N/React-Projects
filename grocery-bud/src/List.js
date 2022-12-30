4dc5770678bc81d56eb71db36f2bf57946a005e0
Chai <developer.chaitra@gmail.com>
2022-12-30 14:27:10 +1300

    Adding Structure

---
+++ grocery-bud/src/List.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { FaEdit, FaTrash } from "react-icons/fa";
+
+//iterate over and display
+const List = () => {
+  return <h2>list component</h2>;
+};
+
+export default List;
